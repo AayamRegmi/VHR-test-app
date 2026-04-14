12a7123291e75507b52283938ec17b6e7a989084
AayamRegmi <aayam.regmi2003@gmail.com>
2026-04-14 20:25:55 +0545

    feat: add insert and findByEmail queries to UserDao

--- app/src/main/java/np/com/aayamregmi/database/dao/UserDao.kt
+++ app/src/main/java/np/com/aayamregmi/database/dao/UserDao.kt
@@ -1,7 +1,17 @@
 package np.com.aayamregmi.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import np.com.aayamregmi.database.entity.UserEntity
 
 @Dao
-interface UserDao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(user: UserEntity)
+
+    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+    suspend fun findByEmail(email: String): UserEntity?
+}
